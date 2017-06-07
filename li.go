6dc168f04d608348f31b1bac3e99f39333a8efea
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-06-07 18:27:32 +0900

    Add list(prefix, dirpath)

--- li.go
+++ li.go
@@ -6,6 +6,29 @@ import (
 	"path/filepath"
 )
 
+func list(prefix string, dirpath string) error {
+	dir, err := os.Open(dirpath)
+	if err != nil {
+		return err
+	}
+	children, err := dir.Readdir(0)
+	if err != nil {
+		return err
+	}
+	for _, child := range children {
+		name := child.Name()
+		if child.IsDir() {
+			err := list(prefix+name+"/", dirpath+"/"+name)
+			if err != nil {
+				return err
+			}
+		} else {
+			fmt.Println(prefix + name)
+		}
+	}
+	return nil
+}
+
 func main() {
 	for _, flag := range os.Args[1:] {
 		if flag[0:2] != "-I" {
